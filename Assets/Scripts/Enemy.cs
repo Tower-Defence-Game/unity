7b489c570bf493f8a636c944faa4d8f5c0b0c8c0
Владимир Курусь <vladimir.kurus@yandex.ru>
2023-03-03 08:16:57 +0700

    refactor(Enemy): remove unused methods

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using Classes.Damage;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     /**
      * Здоровье врага
      */
-    [SerializeField]
-    private float health = 100f;
+    [SerializeField] private float health = 100f;
 
     /**
      * Текущее здоровье врага
      */
-    [SerializeField]
-    private float currentHealth = default;
+    [SerializeField] private float currentHealth = default;
 
     public Vector3 Position => transform.position;
 
     public Collider2D Collider { get; private set; }
-    
+
     void Start()
     {
         // Если текущее здоровье не задано, то присваиваем ему значение по умолчанию
@@ -29,16 +24,11 @@ public class Enemy : MonoBehaviour
         {
             currentHealth = health;
         }
-        
+
         Collider = GetComponent<Collider2D>();
-        
+
         // Проверяем, что у врага есть коллайдер
         Debug.Assert(Collider != null, "Enemy must have a collider");
-    }
-    
-    void FixedUpdate()
-    {
-        
     }
 
     void Die()
@@ -46,11 +36,11 @@ public class Enemy : MonoBehaviour
         // TODO: Сделать смерть через фабрику (и анимацию)
         Destroy(gameObject);
     }
-    
+
     public void TakeDamage(IDamage damage)
     {
         // TODO: Сделать проверку на разные типы урона
-        
+
         currentHealth -= damage.Damage;
         if (currentHealth <= 0)
         {
