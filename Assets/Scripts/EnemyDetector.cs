15fe3365727db1e10b0aca0208af0d4b5bb6a2e1
Владимир Курусь <vladimir.kurus@yandex.ru>
2023-03-03 08:17:45 +0700

    refactor(EnemyDetector): reformat code

--- Assets/Scripts/EnemyDetector.cs
+++ Assets/Scripts/EnemyDetector.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.Serialization;
-
 
 public class EnemyDetector : MonoBehaviour
 {
@@ -12,9 +7,8 @@ public class EnemyDetector : MonoBehaviour
      * Предполагается, что все враги будут находиться на 9-ом слое :)
      */
     private const int EnemyLayerMask = 1 << 9;
-    
-    [SerializeField, Range(1.5f, 10.5f)]
-    private float targetingRange = 1.5f;
+
+    [SerializeField, Range(1.5f, 10.5f)] private float targetingRange = 1.5f;
 
     private Collider2D[] _targetsBuffer = new Collider2D[1];
     [CanBeNull] public Enemy TargetEnemy { get; private set; }
@@ -23,13 +17,15 @@ public class EnemyDetector : MonoBehaviour
     {
         AcquireTarget();
     }
-    
-    private void OnDrawGizmosSelected () {
+
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.yellow;
         Vector3 position = transform.position;
         Gizmos.DrawWireSphere(position, targetingRange);
-        
-        if (TargetEnemy != null) {
+
+        if (TargetEnemy != null)
+        {
             Gizmos.DrawLine(position, TargetEnemy.Position);
         }
     }
@@ -40,7 +36,7 @@ public class EnemyDetector : MonoBehaviour
         {
             return true;
         }
-        
+
         int hits = Physics2D.OverlapCircleNonAlloc(transform.position, targetingRange, _targetsBuffer, EnemyLayerMask);
 
         if (hits == 0)
@@ -48,10 +44,10 @@ public class EnemyDetector : MonoBehaviour
             TargetEnemy = null;
             return false;
         }
-        
+
         TargetEnemy = _targetsBuffer[0].GetComponent<Enemy>();
 
         Debug.Assert(TargetEnemy != null, "TargetEnemy must be Enemy!");
         return true;
-    } 
+    }
 }
