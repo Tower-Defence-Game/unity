9a07a8982c47b9bfc63fb74fa0a98852ebe6ff03
Kugeki <54151036+Kugeki@users.noreply.github.com>
2023-03-31 22:51:56 +0300

    refactor(Towers): move standing logic into 'TowerMapStander'

--- Assets/Scripts/Classes/Tiles/TowerMapManager.cs
+++ Assets/Scripts/Classes/Tiles/TowerMapManager.cs
@@ -1,19 +1,11 @@
-using System.Collections.Generic;
 using Interfaces.ObjectProperties;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class TowerMapManager : MonoBehaviour, IHavePreStart
 {
-    // Tilemap, где есть только тайлы, на которые можно ставить башни
-    [Tooltip("Tilemap, где есть только тайлы, на которые можно ставить башни")] [SerializeField]
-    private Tilemap tilemap;
     [SerializeField] private TowerMapDrawer towerMapDrawer;
-    public Tilemap Tilemap => tilemap;
-    public BaseTower PickedTower { get; set; }
-    public Dictionary<Vector3Int, BaseTower> TowerStands { get; set; } = new();
-    public Vector3 CenteringVector => new(PickedTower.Size.x / 2f - 0.5f, PickedTower.Size.y / 2f - 0.5f);
-    public bool AfterStartDone { get; set; }
+    [SerializeField] private TowerMapStander towerMapStander;
+    private bool AfterStartDone { get; set; }
 
     private void Update()
     {
@@ -21,26 +13,25 @@ public class TowerMapManager : MonoBehaviour, IHavePreStart
 
         if (IsLevelStarted)
         {
-            HideTilemap();
-            AfterStartDone = true;
+            towerMapStander.HideTilemap();
             towerMapDrawer.DestroyPreTower();
+            AfterStartDone = true;
             return;
         }
 
-        if (PickedTower == null)
+        if (towerMapStander.PickedTower == null)
         {
             towerMapDrawer.DestroyPreTower();
             return;
         }
 
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - CenteringVector;
+        var globalMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        towerMapStander.UpdatePosition(globalMousePosition);
 
-        var selectedPosition = tilemap.WorldToCell(mousePosition);
+        var available = towerMapStander.IsTileAvailable();
+        towerMapDrawer.DrawPreTower(towerMapStander.GetTowerCoords(), available, towerMapStander.PickedTower);
 
-        var available = IsTileAvailable(selectedPosition);
-        towerMapDrawer.DrawPreTower(GetTowerCoords(selectedPosition), available, PickedTower);
-
-        if (Input.GetMouseButtonDown(0) && available) PutTower(PickedTower, selectedPosition);
+        if (Input.GetMouseButtonDown(0) && available) towerMapStander.PutTower();
     }
 
     public bool IsLevelStarted { get; set; } = false;
@@ -48,55 +39,6 @@ public class TowerMapManager : MonoBehaviour, IHavePreStart
     public void StartPlacing(BaseTower towerPrefab)
     {
         if (towerMapDrawer.FlyingTower != null) towerMapDrawer.DestroyPreTower();
-        PickedTower = towerPrefab;
-    }
-
-    private Vector3 GetTowerCoords(Vector3Int tilePosition)
-    {
-        return tilemap.GetCellCenterWorld(tilePosition) + CenteringVector;
-    }
-
-    private bool IsTileForTower(Vector3Int tilePosition)
-    {
-        for (var x = 0; x < PickedTower.Size.x; x++)
-        for (var y = 0; y < PickedTower.Size.y; y++)
-        {
-            if (tilemap.GetTile(tilePosition + new Vector3Int(x, y, 0)) == null)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private bool IsTowerStand(Vector3Int tilePosition)
-    {
-        for (var x = 0; x < PickedTower.Size.x; x++)
-        for (var y = 0; y < PickedTower.Size.y; y++)
-        {
-            TowerStands.TryGetValue(tilePosition + new Vector3Int(x, y, 0), out var tower);
-            if (tower != null) return true;
-        }
-
-        return false;
-    }
-
-    private bool IsTileAvailable(Vector3Int tilePosition)
-    {
-        return IsTileForTower(tilePosition) && !IsTowerStand(tilePosition);
-    }
-
-    private void PutTower(BaseTower tower, Vector3Int tilePosition)
-    {
-        TowerStands[tilePosition] = Instantiate(tower, GetTowerCoords(tilePosition), Quaternion.identity);
-        for (var x = 0; x < PickedTower.Size.x; x++)
-        for (var y = 0; y < PickedTower.Size.y; y++)
-            TowerStands[tilePosition + new Vector3Int(x, y, 0)] = TowerStands[tilePosition];
-    }
-
-    private void HideTilemap()
-    {
-        tilemap.GetComponent<TilemapRenderer>().enabled = false;
+        towerMapStander.PickedTower = towerPrefab;
     }
 }
---
+++ Assets/Scripts/Classes/Tiles/TowerMapStander.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class TowerMapStander
+{
+    // Tilemap, где есть только тайлы, на которые можно ставить башни
+    [Tooltip("Tilemap, где есть только тайлы, на которые можно ставить башни")] [SerializeField]
+    private Tilemap tilemap;
+
+    public Tilemap Tilemap => tilemap;
+    public BaseTower PickedTower { get; set; }
+    public Dictionary<Vector3Int, BaseTower> TowerStands { get; set; } = new();
+    public Vector3 CenteringVector => new(PickedTower.Size.x / 2f - 0.5f, PickedTower.Size.y / 2f - 0.5f);
+    public Vector3Int SelectedPosition { get; set; }
+
+    public void UpdatePosition(Vector3 globalMousePosition)
+    {
+        SelectedPosition = GetSelectedPosition(globalMousePosition);
+    }
+
+    public Vector3Int GetSelectedPosition(Vector3 globalMousePosition)
+    {
+        Vector2 mousePosition = globalMousePosition - CenteringVector;
+        return tilemap.WorldToCell(mousePosition);
+    }
+
+    public Vector3 GetTowerCoords()
+    {
+        return GetTowerCoords(SelectedPosition);
+    }
+
+    public Vector3 GetTowerCoords(Vector3Int tilePosition)
+    {
+        return tilemap.GetCellCenterWorld(tilePosition) + CenteringVector;
+    }
+
+    private bool IsTileForTower(Vector3Int tilePosition)
+    {
+        for (var x = 0; x < PickedTower.Size.x; x++)
+        for (var y = 0; y < PickedTower.Size.y; y++)
+            if (tilemap.GetTile(tilePosition + new Vector3Int(x, y, 0)) == null)
+                return false;
+
+        return true;
+    }
+
+    private bool IsTowerStand(Vector3Int tilePosition)
+    {
+        for (var x = 0; x < PickedTower.Size.x; x++)
+        for (var y = 0; y < PickedTower.Size.y; y++)
+        {
+            TowerStands.TryGetValue(tilePosition + new Vector3Int(x, y, 0), out var tower);
+            if (tower != null) return true;
+        }
+
+        return false;
+    }
+
+    public bool IsTileAvailable()
+    {
+        return IsTileAvailable(SelectedPosition);
+    }
+
+    public bool IsTileAvailable(Vector3Int tilePosition)
+    {
+        return IsTileForTower(tilePosition) && !IsTowerStand(tilePosition);
+    }
+
+    public void PutTower()
+    {
+        PutTower(PickedTower, SelectedPosition);
+    }
+
+    public void PutTower(BaseTower tower, Vector3Int tilePosition)
+    {
+        TowerStands[tilePosition] = Object.Instantiate(tower, GetTowerCoords(tilePosition), Quaternion.identity);
+        for (var x = 0; x < PickedTower.Size.x; x++)
+        for (var y = 0; y < PickedTower.Size.y; y++)
+            TowerStands[tilePosition + new Vector3Int(x, y, 0)] = TowerStands[tilePosition];
+    }
+
+
+    public void HideTilemap()
+    {
+        tilemap.GetComponent<TilemapRenderer>().enabled = false;
+    }
+}
