3aa52cc92c01f7971b031d5b87bb693906eaacd3
Kugeki <54151036+Kugeki@users.noreply.github.com>
2023-05-16 22:28:24 +0300

    Добавил механику звёзд у башен

--- Assets/Scripts/Classes/Tiles/Cell/Cell.cs
+++ Assets/Scripts/Classes/Tiles/Cell/Cell.cs
@@ -9,9 +9,10 @@ namespace Classes.Tiles.Cell
     {
         private readonly GameObject _gameObject;
         private readonly Image _image;
-        private readonly TextMeshProUGUI _countText;
         private readonly Button _button;
-        private int _count;
+        
+        private readonly TextMeshProUGUI _countText;
+        private readonly TextMeshProUGUI _starsText;
 
         public int Count
         {
@@ -19,11 +20,21 @@ namespace Classes.Tiles.Cell
             set => _countText.text = value.ToString();
         }
 
+        public int Stars
+        {
+            get => int.Parse(_starsText.text);
+            set => _starsText.text = value.ToString();
+        }
+
         public Sprite Sprite { set => _image.sprite = value; }
 
         public Color Color { set => _image.color = value; }
 
-        public bool Interactable { set => _button.interactable = value; }
+        public bool Interactable
+        {
+            get => _button.interactable;
+            set => _button.interactable = value;
+        }
 
         private Cell(GameObject cellPrefab, GameObject cellsContent)
         {
@@ -31,18 +42,23 @@ namespace Classes.Tiles.Cell
             _gameObject.transform.SetParent(cellsContent.transform, false);
 
             _image = _gameObject.GetComponent<Image>();
-            _countText = _gameObject.GetComponentInChildren<TextMeshProUGUI>();
             _button = _gameObject.GetComponent<Button>();
+            
+            var texts = _gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+            _countText = texts[0];
+            _starsText = texts[1];
         }
 
         public Cell(GameObject cellPrefab, GameObject cellsContent, TowerWithCount towerWithCount) 
             : this(cellPrefab, cellsContent)
         {
-            var spriteRenderer = towerWithCount.Tower.GetComponentInChildren<SpriteRenderer>();
+            var tower = towerWithCount.Tower;
+            var spriteRenderer = tower.GetComponentInChildren<SpriteRenderer>();
             
             Sprite = spriteRenderer.sprite;
             Color = spriteRenderer.color;
             Count = towerWithCount.Count;
+            Stars = tower.Stars;
         }
 
         public void AddOnClickListener(UnityAction call)
